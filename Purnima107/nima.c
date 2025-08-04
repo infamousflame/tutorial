348f9b33444151bf42951f97b80b807ad4588f62
Krishna Ranchhod <67448147+infamousflame@users.noreply.github.com>
2025-08-04 13:54:07 +1200

    Return of the segfault.

--- Purnima107/nima.c
+++ Purnima107/nima.c
@@ -3,11 +3,12 @@
 
 int main(void) {
   printf("Hello World");
-  void *thing = malloc(2147483648);
+  void *youngling = malloc(2147483648);
   printf("Nuh uh your program with FAIL xD.");
   printf(" ; (   ");
   printf("Damn I think I've forgotten C already");
-  free(thing);
+  free(youngling);
   printf("Be free youngling!");
+ *youngling = 0;
   return 1;
 }
