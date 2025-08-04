3cfd2df14158c795dda624824c0db3f435eb7671
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2025-08-04 13:48:06 +1200

    関東

--- Purnima107/nima.c
+++ Purnima107/nima.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (void) {
-	printf("Hello World");
-	void *thing = malloc(2147483648);
-	printf("Nuh uh your program with FAIL xD.");
-	printf(" ; (   "); 
-	printf("Damn I think I've forgotten C already"); 
-	return 1; 
+int main(void) {
+  printf("Hello World");
+  void *thing = malloc(2147483648);
+  printf("Nuh uh your program with FAIL xD.");
+  printf(" ; (   ");
+  printf("Damn I think I've forgotten C already");
+  free(thing);
+  printf("Be free youngling!");
+  return 1;
 }
